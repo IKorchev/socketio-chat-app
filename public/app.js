6346288aa2f778edca40062527d2eba2f3defed2
Korchev <Korchev94@gmail.com>
2021-02-22 17:19:49 +0000

    changes. Fixed bug with colors, added time when the message was sent

--- public/app.js
+++ public/app.js
@@ -7,32 +7,39 @@ const chosenColor = document.querySelector(".colorInput")
 
 const username = prompt("Your name please ?")
 
-const formatData = (data, h5ClassName, listItemClassName) => {
+const formatData = (data, color) => {
+  const time = {
+    hour: new Date().getHours(),
+    minutes: new Date().getMinutes(),
+  }
   output.innerHTML += `
-  <li class="list-group-item ${listItemClassName}">
-    <h5 class="d-inline ${h5ClassName}">${data.name} </h5><i>says:</i>
-    <p class="mx-3 my-1">${data.msg}</p>
+  <li class="list-group-item mt-1 a${color}">
+  <div class="d-flex flex-row justify-content-between">
+    <div>
+      <h4 class="d-inline a${color}">${data.name} </h4>
+      <i class="fs-lg text-dark">says:</i>
+    </div>
+    <i muted class="text-dark">${time.hour}:${time.minutes}</i>
+  </div>
+    <p class="mx-3 my-1 text-dark">${data.msg}</p>
   </li>`
-  const array = document.querySelectorAll(`.${h5ClassName}`)
+  const array = document.querySelectorAll(`.a${color}`)
   array.forEach((item) => {
-    item.style.color = data.color
-  })
-  const listItems = document.querySelectorAll(`.${listItemClassName}`)
-  listItems.forEach((item) => {
-    item.style.borderColor = data.color
+    item.style.color = `#${color}`
+    item.style.borderColor = `#${color}`
   })
 }
 
 sendBtn.addEventListener("click", (e) => {
   e.preventDefault()
   const data = {
-    color: chosenColor.value,
+    color: chosenColor.value.slice(1, 7),
     name: username,
     msg: message.value,
   }
   if (data.msg !== "" && data.name !== "") {
+    formatData(data, data.color)
     socket.emit("client-message", data)
-    formatData(data, "myName", "list-item")
     message.value = ""
   } else {
     alert("Something went wrong! Please make sure to type your name and message to send!")
@@ -40,5 +47,5 @@ sendBtn.addEventListener("click", (e) => {
 })
 
 socket.on("server-message", (data) => {
-  formatData(data, "nameFromServer", "other-list-item")
+  formatData(data, data.color)
 })
