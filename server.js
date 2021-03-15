7b8bca39c9a7081ca5b433e2cbca23badc20bed4
Korchev <Korchev94@gmail.com>
2021-03-15 21:20:57 +0000

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path")
 const express = require("express")
 const app = express()
 const server = require("http").Server(app)
-const PORT = 5500
+const PORT = process.env.PORT || 80
 const io = require("socket.io")(server)
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
